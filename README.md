a020559dddac57cff5e7e76f86fa0f7db3f12f01
apip <apip@gitlab.lab>
2020-08-24 08:14:25 +0700

    POST method

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # REST-API_crud
 build a REST API with Node JS and Express.
 
+step by step ::
 - npm init -y
+    to installing package.json
 - install Node JS
     https://nodejs.org/en/
-- install Express JS
+- npm install --save express
+    to installing express js
     https://expressjs.com/
-    npm install --save express
 - npm install --save-dev nodemon
-    for quick compiling/auto running when click CTRL+S
+    to quick compiling/auto running when click CTRL+S,
+    nodemon is a tool that helps develop node.js based applications by automatically restarting the node application when file changes in the directory are detected.
+    https://www.npmjs.com/package/nodemon
+- npm install uuid
+    to create a random uuid
+    https://www.npmjs.com/package/uuid
--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,32 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 const users = [
-    {
-        firstName: "Apip",
-        lastName: "kurniawan",
-        age: 28
-    },
-    {
-        firstName: "Aah",
-        lastName: "Siti Robiah",
-        age: 27
-    }
+    // {
+    //     firstName: "Apip",
+    //     lastName: "kurniawan",
+    //     age: 28
+    // },
+    // {
+    //     firstName: "Aah",
+    //     lastName: "Siti Robiah",
+    //     age: 27
+    // }
 ];
 
 /* all routes in here are starting with /users */
 // GET
 router.get('/', (req, res) => {
-    console.log(users);
     res.send(users);
 });
 // POST
+router.post('/', (req, res) => {
+    const user = req.body;
+    const userWithId = { ...user, id: uuidv4() }; // menambahkan key dan value baru pada json dengan spread separator(bukan array)
+    users.push(userWithId);
+    res.send(`User with the name ${user.firstName} added to the Database!`);
+});
 
 
 export default router;
